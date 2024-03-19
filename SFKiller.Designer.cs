195ac2740071f995bf7498ee5c0adf56ba380ddc
Doc :) <mr.sheep193@gmail.com>
2024-03-19 23:42:38 +0100

    SFKiller V0.1
    SFKiller debloats your device of unnecessary .sfk (Vegas Pro) files

---
+++ SFKiller.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SFKiller
+{
+    partial class SFKiller
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SFKiller));
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            progressBar1 = new ProgressBar();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = SystemColors.InactiveCaptionText;
+            comboBox1.ForeColor = SystemColors.Window;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 184);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(117, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Select Drive";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Exo 2", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(12, 157);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 24);
+            label1.TabIndex = 1;
+            label1.Text = "Drive to clear";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(135, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(237, 23);
+            button1.TabIndex = 2;
+            button1.Text = "NUKE IT!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.BackColor = Color.FromArgb(32, 32, 32);
+            progressBar1.Location = new Point(12, 220);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(360, 29);
+            progressBar1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.SFKillerImage;
+            pictureBox1.Location = new Point(34, 50);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(318, 76);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Cursor = Cursors.Hand;
+            label2.Font = new Font("Orbitron", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Firebrick;
+            label2.Location = new Point(343, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 26);
+            label2.TabIndex = 5;
+            label2.Text = "X";
+            label2.Click += label2_Click;
+            // 
+            // SFKiller
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(25, 25, 25);
+            ClientSize = new Size(384, 261);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(progressBar1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            Name = "SFKiller";
+            Text = "SFKiller";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Button button1;
+        private ProgressBar progressBar1;
+        private PictureBox pictureBox1;
+        private Label label2;
+    }
+}
---
+++ SFKiller.cs
@@ -0,0 +1,141 @@
+namespace SFKiller
+{
+    public partial class SFKiller : Form
+    {
+
+        private bool isDragging = false;
+        private Point lastCursor;
+        private Point lastForm;
+
+        public SFKiller()
+        {
+            InitializeComponent();
+            PopulateDriveComboBox();
+
+            this.MouseDown += SFKiller_MouseDown;
+            this.MouseUp += SFKiller_MouseUp;
+            this.MouseMove += SFKiller_MouseMove;
+        }
+
+        private void PopulateDriveComboBox()
+        {
+            comboBox1.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                comboBox1.Items.Add(drive.Name);
+            }
+
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Select the Drive
+            string selectedDrive = comboBox1.SelectedItem.ToString();
+
+            // Show loading bar
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 30;
+            progressBar1.Visible = true;
+
+            // Search for the .sfk files
+            List<string> sfkFiles = await SearchSFKFilesAsync(selectedDrive);
+
+            // Stop loading bar (or specifically the animation)
+            progressBar1.MarqueeAnimationSpeed = 0;
+
+            // Count the number of .sfk files found
+            int sfkFileCount = sfkFiles.Count;
+
+            // Prompt for Deleting
+            DialogResult result = MessageBox.Show($"Found {sfkFileCount} .sfk files on drive {selectedDrive}. Do you want to delete them?",
+                                                   "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Delete Files
+                DeleteSFKFiles(sfkFiles);
+                MessageBox.Show("Files deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Abrort
+                MessageBox.Show("Operation cancelled.", "Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void DeleteSFKFiles(List<string> sfkFiles)
+        {
+            foreach (string file in sfkFiles)
+            {
+                File.Delete(file);
+            }
+        }
+
+        private Task<List<string>> SearchSFKFilesAsync(string directory)
+        {
+            return Task.Run(() =>
+            {
+                List<string> sfkFiles = new List<string>();
+                SearchSFKFiles(directory, ref sfkFiles);
+                return sfkFiles;
+            });
+        }
+
+        private void SearchSFKFiles(string directory, ref List<string> sfkFiles)
+        {
+            try
+            {
+                // Search for .sfk files in the directory
+                string[] files = Directory.GetFiles(directory, "*.sfk");
+                sfkFiles.AddRange(files);
+
+                // Search subdirectories
+                string[] subdirectories = Directory.GetDirectories(directory);
+                foreach (string subdirectory in subdirectories)
+                {
+                    SearchSFKFiles(subdirectory, ref sfkFiles);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Skip this directory if access is denied
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SFKiller_MouseDown(object sender, MouseEventArgs e)
+        {
+            // On Mouse Press get position and set dragging
+            isDragging = true;
+            lastCursor = Cursor.Position;
+            lastForm = this.Location;
+        }
+
+        private void SFKiller_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void SFKiller_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Calculate differenc in mouse position
+            if (isDragging)
+            {
+                Point currentCursor = Cursor.Position;
+                int deltaX = currentCursor.X - lastCursor.X;
+                int deltaY = currentCursor.Y - lastCursor.Y;
+                this.Location = new Point(lastForm.X + deltaX, lastForm.Y + deltaY);
+            }
+        }
+    }
+}
